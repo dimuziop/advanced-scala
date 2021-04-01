53beb43b4a00d092f481ed1692a4ebae62c31594
Patricio Di Muzio <padimuzio@gmail.com>
2021-04-01 18:48:29 +0200

    part 3 done

--- src/lectures/part3concurrency/FuturesPromises.scala
+++ src/lectures/part3concurrency/FuturesPromises.scala
@@ -1,6 +1,7 @@
 package lectures.part3concurrency
 
-import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future, Promise}
 import scala.util.{Failure, Random, Success}
 // important for futures
 //import scala.concurrent.ExecutionContext.global
@@ -87,7 +88,7 @@ object FuturesPromises extends App {
       }
     case Failure(exception) => exception.printStackTrace()
   })*/
- // CALLBACK FROM HELL!!!!!
+  // CALLBACK FROM HELL!!!!!
   //Thread.sleep(3000)
 
   // funcitonal composition of futures
@@ -98,7 +99,7 @@ object FuturesPromises extends App {
 
   val zucksBestFriendRestricted = markBestFriend.filter(profile => profile.name.startsWith("Z"))
 
- // for-comprehesions
+  // for-comprehesions
 
   for {
     mark <- SocialNetwork.fetchProfile("fb.id.1-zuck")
@@ -109,13 +110,73 @@ object FuturesPromises extends App {
 
   // fallbacks
 
-  val aProfileNoMatherWhat = SocialNetwork.fetchProfile("unknown id").recover{
+  val aProfileNoMatherWhat = SocialNetwork.fetchProfile("unknown id").recover {
     case error: Throwable => Profile("fb.id.3-dummy", "Forever Alone")
   }
 
-  val aFetchProfileNoMatherWhat = SocialNetwork.fetchProfile("unknown id").recoverWith{
+  val aFetchProfileNoMatherWhat = SocialNetwork.fetchProfile("unknown id").recoverWith {
     case error: Throwable => SocialNetwork.fetchProfile("fb.id.3-dummy")
   }
 
   val fallbackResult = SocialNetwork.fetchProfile("unknown id").fallbackTo(SocialNetwork.fetchProfile("fb.id.3-dummy"))
+
+  // inline backing app (transactional stuff)
+
+  case class User(name: String)
+
+  case class Transaction(sender: String, receiver: String, amount: Double, status: String)
+
+  object BankingApp {
+    val name = "Rock the JVM banking"
+
+    def fetchUser(name: String): Future[User] = Future {
+      Thread.sleep(500)
+      User(name)
+    }
+
+    def createTransaction(user: User, merchantName: String, amount: Double): Future[Transaction] = Future {
+      Thread.sleep(1000)
+      Transaction(user.name, merchantName, amount , status = "SUCCESS")
+    }
+
+    def purchase(username: String, item: String, merchantName: String, cost:Double): String = {
+      // fetch user from db
+      // create a transaction
+      //WAIT for the transaction to finish
+      val transactionStatusFuture = for {
+        user <- fetchUser(name)
+        transaction <- createTransaction(user, merchantName, cost)
+      } yield transaction.status
+
+      Await.result(transactionStatusFuture, 2.seconds)// implicit conversions -> pimp my library
+
+    }
+  }
+
+  println(BankingApp.purchase("Patricio", "Ferrari f50", "Enzo", 250000))
+
+  // promises
+
+  val promise = Promise[Int]() // "controller" over a future
+  val future = promise.future
+
+  // thread 1 - "consumer"
+  future.onComplete {
+    case Success(r) => println("[consumer] I've received " + r)
+  }
+
+  // thread 2 - "producer"
+  val producer = new Thread(() => {
+    println("[producer] crunching numbers...")
+    Thread.sleep(500)
+    // "fulfilling" the promise
+    promise.success(42)
+    println("[producer] done")
+  })
+
+  producer.start()
+  Thread.sleep(1000)
+
+
+
 }
