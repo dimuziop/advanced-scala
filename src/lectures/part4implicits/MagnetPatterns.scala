b8895fff291c4ce431dad981f1cedc0fc9f52c0a
Patricio Di Muzio <padimuzio@gmail.com>
2021-04-05 19:00:29 +0200

    the magnet patern

---
+++ src/lectures/part4implicits/MagnetPatterns.scala
@@ -0,0 +1,161 @@
+package lectures.part4implicits
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+/**
+ * User: patricio
+ * Date: 5/4/21
+ * Time: 07:41
+ */
+object MagnetPatterns extends App {
+
+
+  // method overloading
+
+  class P2PRequest
+  class P2PResponse
+  class Serializer[T]
+
+  trait Actor {
+    def receive(statusCode: Int): Int
+    def receive(request: P2PRequest): Int
+    def receive(response: P2PResponse): Int
+    // def receive[T](message: T)(implicit serializer: Serializer[T]): Int
+    def receive[T: Serializer](message: T): Int
+    def receive[T: Serializer](message: T, statusCode: Int): Int
+    def receive(future: Future[P2PRequest]): Int
+    // def receive(future: Future[P2PResponse]): Int ---- Type Erasure
+    // lots of overload
+  }
+
+  /*
+  1 - Type Erasure
+  2 - lifting does not work for all overloads
+    val receiveFV = receive _ // ?!
+
+  3 - code duplication
+  4 - type inference and default args
+      actor.receive(?!)
+   */
+
+  trait MessageMagnet[Result] {
+    def apply(): Result
+  }
+
+  def receive[R](magnet: MessageMagnet[R]): R = magnet()
+
+  implicit class FromP2PRequest(request: P2PRequest) extends MessageMagnet[Int] {
+    override def apply(): Int = {
+      // logic for handling a p2p request
+      println("P2p request handling")
+      42
+    }
+  }
+
+  implicit class FromP2PResponse(response: P2PResponse) extends MessageMagnet[Int] {
+    override def apply(): Int = {
+      // logic for handling a P2PResponse
+      println("P2p Response handling")
+      24
+    }
+  }
+
+  receive(new P2PRequest)
+  receive(new P2PResponse)
+
+  //1 - no more type erasure problems
+  implicit class FromResponseFuture(response: Future[P2PResponse]) extends MessageMagnet[Int] {
+    override def apply(): Int = {
+      // logic for handling a P2PResponse
+      println("P2p Response handling future")
+      24
+    }
+  }
+
+  implicit class FromRequestFuture(request: Future[P2PRequest]) extends MessageMagnet[Int] {
+    override def apply(): Int = {
+      // logic for handling a P2PResponse
+      println("P2p Response handling future")
+      42
+    }
+  }
+
+  println(receive(Future {
+    new P2PRequest
+  }))
+
+  println(receive(Future {
+    new P2PResponse
+  }))
+
+  // 2 -lifting works
+  trait MathLib {
+    def add1(x: Int): Int = x + 1
+    def add1(s: String): Int = s.toInt + 1
+  }
+  // "magnetize"
+  trait AddMagnet {
+    def apply(): Int
+  }
+
+  def add1(magnet: AddMagnet): Int = magnet()
+
+  implicit class AddInt(x: Int) extends AddMagnet {
+    override def apply(): Int = x + 1
+  }
+
+  implicit class AddString(s: String) extends AddMagnet {
+    override def apply(): Int = s.toInt + 1
+  }
+
+  val addFV = add1 _
+  println(addFV(1))
+  println(addFV("3"))
+
+  //  val receiveFV = receive _
+  //  receiveFV(new P2PResponse)
+
+  /*
+    Drawbacks
+    1 - verbose
+    2 - harder to read
+    3 - you can't name or place default arguments
+    4 - call by name doesn't work correctly
+    (exercise: prove it!) (hint: side effects)
+   */
+
+  class Handler {
+    def handle(s: => String) = {
+      println(s)
+      println(s)
+    }
+    // other overloads
+  }
+
+  trait HandleMagnet  {
+    def apply(): Unit
+  }
+
+  def handle(magnet: HandleMagnet) = magnet()
+
+  implicit class StringHandle(s: => String) extends HandleMagnet {
+    override def apply(): Unit = {
+      println(s)
+      println(s)
+    }
+  }
+
+  def sideEffectMethod(): String = {
+    println("Hello, Scala")
+    "hahaha"
+  }
+
+  //  handle(sideEffectMethod())
+  handle {
+    println("Hello, Scala")
+    new StringHandle("magnet")
+  }
+  // careful!
+
+}
